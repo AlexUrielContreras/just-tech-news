3bf0a9918617f43591f7445a3e3441cb7dda54c1
Alex Contreras <alex.code29@gmail.com>
2022-06-29 21:23:22 -0500

    added comments

--- server.js
+++ server.js
@@ -6,13 +6,16 @@ const sequelize = require('./config/connections');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// express middleware
+// parses incoming json request and this data in a req.body
 app.use(express.json());
+//  parses incoming requests with urlencoded payloads and is based on body-parser
 app.use(express.urlencoded({ extanded: true }));
 
 // turn on routes
 app.use(routes);
 
 // turn on connection to db and server 
-sequelize.sync({ force: false}).then(() => {
+sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log(`Now Listening on ${PORT}`))
 })
