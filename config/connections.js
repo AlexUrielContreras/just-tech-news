3c380b93b22b754a9d6237861e8bf48fd66fcc66
Alex Contreras <alex.code29@gmail.com>
2022-07-03 22:40:30 -0500

    bug: fixed connection to db

--- config/connections.js
+++ config/connections.js
@@ -1,12 +1,14 @@
 // import the Sequelize constructor from the libary 
-let sequelize; 
+const Sequelize = require('sequelize')
 
 require('dotenv').config();
+
+let sequelize; 
 
 if (process.env.JAWSDB_URL) {
     sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-    seqelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
         host: 'localhost',
         dialect: 'mysql',
         post: 3306
