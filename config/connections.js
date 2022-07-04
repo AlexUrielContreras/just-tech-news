8f21d7ba594a9b6ff8ce46372397b94261a1ae36
Alex Contreras <alex.code29@gmail.com>
2022-07-03 22:36:26 -0500

    refactor: switch to jawsdb connection

--- config/connections.js
+++ config/connections.js
@@ -1,15 +1,16 @@
 // import the Sequelize constructor from the libary 
-const Sequelize = require('sequelize');
+let sequelize; 
 
 require('dotenv').config();
 
-// create connection to our database
-// grabs the credentials from the .env file
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-});
-
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+    seqelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+        host: 'localhost',
+        dialect: 'mysql',
+        post: 3306
+    });
+}
 
 module.exports = sequelize;
